81369f0f0422e008c45caebb7aca54d87ea00309
Viktoriia <98652392+fullstack2002@users.noreply.github.com>
2024-10-25 14:51:24 +0200

    hw 5

--- .vscode/lesson 5/assignment1.py
+++ .vscode/lesson 5/assignment1.py
@@ -2,13 +2,19 @@ import machine
 from time import sleep
 
 analogInput = 28
-# analog pin
+# analog pin where the potentiometer is connected
+# it will be used to read the voltage from the potentiometer
+
 potentiometer = machine.ADC(analogInput)
-# potentiometer object 
 # ADC - analog to digital converter
+# potentiometer object
+# it allows to read analog values from the potentiometer
+
 
 while True:
     potentiometerVal = potentiometer.read_u16()
+    # read the current value from the potentiometer as a 16-bit unsigned integer (0 to 65535)
     voltage = (3.3 / 65535) * potentiometerVal 
+    # convert the raw ADC value to voltage (in volts) based on a 3.3V reference
     print(voltage)
     sleep(.5)
--- .vscode/lesson 5/assignment2.py
+++ .vscode/lesson 5/assignment2.py
@@ -1,2 +1,15 @@
 # hw
 # all the way to the left, read 100, all the way to the right read 0
+
+import machine
+from time import sleep
+
+analogInput = 28
+potentiometer = machine.ADC(analogInput)
+
+while True:
+    potentiometerVal = potentiometer.read_u16()
+    voltage = (3.3 / 65535) * potentiometerVal
+    percentage = 100 * (1 - voltage / 3.3)
+    print(int(percentage))
+    sleep(0.5)
