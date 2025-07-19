49b467068fd938fc9c8734d914297fe546d50fcc
Viktoriia <vika.vinnikova@gmail.com>
2025-07-19 18:52:33 +0200

    lcd dht11

--- lesson 22/hw22_lcd_dht11.py
+++ lesson 22/hw22_lcd_dht11.py
@@ -1,44 +1,41 @@
 # displayd tempF, tempC, humidity on LCD
 
 from machine import Pin
-from lcd1602 import LCD
 import utime as time
 from dht import DHT11
+from lcd1602 import LCD
 
+lcd=LCD()
+ 
 dataPin = 16
-myPin = Pin(dataPin)
+myPin = Pin(dataPin,Pin.OUT,Pin.PULL_DOWN)
 sensor = DHT11(myPin)
-
+ 
 butPin = 15
-myButton = Pin(butPin, Pin.IN, Pin.PULL_UP)
-butStateNow = 1
-butStateOld = 1
+myButton = Pin(butPin,Pin.IN,Pin.PULL_UP)
 
 tempUnitC = True
-
-lcd = LCD()
-lcd.clear()
+buttonState = 1
+buttonStateOld = 1
 
 while True:
-    butStateNow = myButton.value()
-    if butStateNow == 1 and butStateOld == 0:
+    buttonState = myButton.value()
+    if buttonStateOld == 0 and buttonState == 1:
         tempUnitC = not tempUnitC
-
-    butStateOld = butStateNow
-
     try:
         sensor.measure()
-        tempC = sensor.temperature()
-        tempF = (tempC * 9/5) + 32
-        humidity = sensor.humidity()
-        lcd.clear()
-    
-        if tempUnitC == True:
-            lcd.write(0, 0, "temp: " + str(tempC) + "\xDF" + "C")
-            lcd.write(0, 1, "humidity: " + str(humidity) + "%")
-        else:
-            lcd.write(0, 0, "temp: " + str(tempF) + "\xDF" + "F")
-            lcd.write(0, 1, "humidity: " + str(humidity) + "%")
-    except OSError as e:
-        print("\r", "Sensor error:", e, end='')
-    time.sleep(0.5)
+    except:
+        pass
+
+    tempC = sensor.temperature()
+    tempF = tempC * 9/5 + 32
+    hum = sensor.humidity()
+
+    if tempUnitC == True:
+        lcd.write(0,0,'Temp: '+str(tempC)+'\xDF'+'C  ')
+        lcd.write(0,1,"Humidity: "+str(hum)+'%')
+    if tempUnitC == False:
+        lcd.write(0,0,'Temp: '+str(tempF)+'\xDF'+'F')
+        lcd.write(0,1,"Humidity: "+str(hum)+'%')
+    time.sleep(.1)
+    buttonStateOld = buttonState
